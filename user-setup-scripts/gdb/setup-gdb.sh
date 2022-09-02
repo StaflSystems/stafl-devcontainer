c85877a0f272e44fb1a5f1640be10db4d88357f9
STAFLSYSTEMS\ben.hefner <ben.hefner@staflsystems.com>
2022-09-02 10:55:32 -0700

    add user setup files

---
+++ user-setup-scripts/gdb/setup-gdb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /user-setup-scripts/gdb/.gdbinit ~
---
+++ user-setup-scripts/git-completion/setup-git-completion.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /user-setup-scripts/git-completion/git-completion.bash ~/.bash/
+echo "source ~/.bash/git-completion.bash" >> ~/.bashrc
