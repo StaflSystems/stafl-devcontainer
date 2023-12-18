16b2c108566ebe2aab4ccb28d459b831ee1b5c87
Ben Ng <xbenng@users.noreply.github.com>
2023-12-18 09:52:43 -0800

    CI Build and Push & Container Upgrades (#26)
    Adds docker build and push image to CI pipeline
    
    Docker Image Upgrades
    - Adds ksh, bash-completion, ripgrep
    - Updates nodejs installation method
    - Updates git 
    - Fixes git-lfs installation
    - Adds graphite-cli

---
+++ stafl-devcontainer/user-setup-scripts/setup-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script should be run upon first user login
+
+# Docker containers derived from stafl-devcontainer may append to this script to install user setup actions.
+
+# correct line endings
+find -path '/user-setup-scripts/*' | xargs dos2unix
+
+. /user-setup-scripts/gdb/setup-gdb.sh
