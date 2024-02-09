e616310974bcdf45915897baa58dc43b2a20d4ce
Thomas Willson <thomas.willson@staflsystems.com>
2024-02-09 09:20:17 -0800

    Add RTOS plugins.

--- stafl-devcontainer/user-setup-scripts/setup-all.sh
+++ stafl-devcontainer/user-setup-scripts/setup-all.sh
@@ -5,6 +5,8 @@
 # Docker containers derived from stafl-devcontainer may append to this script to install user setup actions.
 
 # correct line endings
-find -path '/user-setup-scripts/*' | xargs dos2unix
+find -path '/user-setup-scripts/*.sh' | xargs dos2unix
+find -path '/user-setup-scripts/*.gdbinit' | xargs dos2unix
 
 . /user-setup-scripts/gdb/setup-gdb.sh
+. /user-setup-scripts/rtos-views/setup-rtos-views.sh
